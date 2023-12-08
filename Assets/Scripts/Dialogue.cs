befc4636958e1d7b89bc620e0053f6a2c15892b6
Shestakov Nikolay <workjustghostw1ng@gmail.com>
2023-12-08 17:02:39 +0300

    Import articy draft and add dialogue manager

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using TMPro;
+using Articy.Unity;
+using Articy.Unity.Interfaces;
+using System.Collections.Generic;
+using Articy.Testproect;
+
+[RequireComponent(typeof(ArticyFlowPlayer))]
+public class Dialogue : MonoBehaviour, IArticyFlowPlayerCallbacks
+{
+	[SerializeField] private ArticyDebugBranch _brunchTemplate;
+	[SerializeField] private CanvasGroup _dialoguePanel;
+	[SerializeField] private RectTransform _branchesLayout;
+	[SerializeField] private TMP_Text _dialogueText;
+	[SerializeField] private TMP_Text _dialogueSpeaker;
+
+	private bool _isDialogActive = false;
+	private ArticyFlowPlayer _flowPlayer;
+
+	public bool IsDialogActive => _isDialogActive;
+
+	private void Start()
+	{
+		_flowPlayer = GetComponent<ArticyFlowPlayer>();
+	}
+
+	private void ChangePanelActivity(bool isActive)
+	{
+		_dialoguePanel.interactable = isActive;
+		_dialoguePanel.blocksRaycasts = isActive;
+		if (isActive)
+			_dialoguePanel.alpha = 1;
+		else
+			_dialoguePanel.alpha = 0;
+	}
+
+	public void StartDialogue(string dialogueLine, string dialogueSpeaker)
+	{
+		_isDialogActive = true;
+		ChangePanelActivity(_isDialogActive);
+
+		_dialogueText.text = dialogueLine;
+		_dialogueSpeaker.text = dialogueSpeaker;
+	}
+
+	public void CloseDialogue()
+	{
+		_isDialogActive = false;
+		ChangePanelActivity(_isDialogActive);
+	}
+
+	public void OnFlowPlayerPaused(IFlowObject aObject)
+	{
+		if (aObject is IObjectWithSpeaker objectWithSpeaker)
+		{
+			if(objectWithSpeaker != null)
+			{
+				Entity speakerEntity = objectWithSpeaker.Speaker as Entity;
+				if(speakerEntity != null)
+				{
+					_dialogueSpeaker.text = speakerEntity.DisplayName;
+				}
+			}
+		}
+		else
+			_dialogueSpeaker.text = string.Empty;
+
+		if (aObject is IObjectWithText objectWithText)
+			_dialogueText.text = objectWithText.Text;
+		else if (aObject is IObjectWithLocalizableText objectWithLocalizableText)
+			_dialogueText.text = objectWithLocalizableText.Text;
+		else
+			_dialogueText.text = string.Empty;
+	}
+
+	public void OnBranchesUpdated(IList<Branch> aBranches)
+	{
+		ClearAllBranches();
+
+		foreach (Branch branch in aBranches)
+		{
+			if (!branch.IsValid) continue;
+
+			var button = Instantiate(_brunchTemplate);
+			RectTransform rectTransform = button.GetComponent<RectTransform>();
+			rectTransform.SetParent(_branchesLayout, false);
+
+			var branchButton = button.GetComponent<ArticyDebugBranch>();
+			if (branchButton == null)
+				branchButton = button.gameObject.AddComponent<ArticyDebugBranch>();
+			branchButton.AssignBranch(_flowPlayer, branch);
+		}
+	}
+
+	private void ClearAllBranches()
+	{
+		foreach (Transform child in _branchesLayout)
+		{
+			Destroy(child.gameObject);
+		}
+	}
+}
